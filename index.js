f73cf45df8fdf0d0546c98889ad33ada7551b9a0
Evgenii Klimov <disturh@gmail.com>
2018-05-20 23:28:13 +0300

    added chalk to all messages. Program is almost ready

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ let CONFIG;
 const getConfig = (args, resolve) => {
   const [configPath] = args;
   const getDefaultConfig = () => {
-    LOG(`Default config created here ${configPath}`);
+    LOG(CH`{cyanBright The default Config is created here:} ${configPath}`);
     return FILE.read('default-config.json');
   };
 
@@ -36,13 +36,13 @@ const preparations = (args, resolve) => {
 
   // Test 1: is git installed?
   if (!SHELL.which('git')) {
-    ERR('Error: git is not found');
+    ERR(CH`{red Error: git is not found}`);
     SHELL.exit(1);
   }
 
   // Test 2: is Project name specified?
   if (!PROJECT_NAME) {
-    LOG('Config editing mode...');
+    LOG(CH`{cyanBright Config editing mode...}`);
     OPN(CONFIG_FILE);
     SHELL.exit(0);
   }
@@ -58,7 +58,7 @@ const preparations = (args, resolve) => {
       if (answers.toClear) {
         const isRemoved = SHELL.rm('-rf', PROJECT_PATH);
         if (isRemoved.code) {
-          ERR(`Can not remove ${PROJECT_PATH}. It's possibly locked`);
+          ERR(CH`{red Ensure the directory is not used by another application then retry.}`);
           SHELL.exit(0);
         }
 
@@ -87,14 +87,14 @@ const getSourceUrl = (args, resolve) => {
   // debugger;
   if (isAlias) {
     if (knownSource) {
-      LOG(`Source Project's URI is ${sourceUrl}`);
+      LOG(CH`{cyanBright Source Project's URI is:} ${sourceUrl}`);
       resolve(sourceUrl);
     } else {
-      ERR(`Unknown alias ${sourceAlias}`);
+      LOG(CH`{yellow Unknown alias:} ${sourceAlias}`);
       const firstSource = sources[0];
 
       if (config.firstForced) {
-        LOG(`Using the first source (${firstSource.alias}: ${firstSource.url}) ...`);
+        LOG(CH`{cyanBright Using the first source repo} ({yellow ${firstSource.alias}:} ${firstSource.url})`);
         resolve(firstSource.url);
       } else {
         const questions = [{
@@ -112,7 +112,7 @@ const getSourceUrl = (args, resolve) => {
       }
     }
   } else if (knownSource) {
-    LOG(`This URI had been used already. You may specify only alias ${sourceAlias} instead`);
+    LOG(CH`{yellow This URI had been used already.} You may specify only alias ${sourceAlias} instead`);
     resolve(sourceUrl);
   } else {
     // The new alias will be added to the config. Mutation!
@@ -120,7 +120,7 @@ const getSourceUrl = (args, resolve) => {
     const alias = sourceAlias || `${aliasBase}${config.sources.length}`;
     config.sources.push({ alias, url: sourceUrl });
     FILE.write(CONFIG_FILE, JSON.stringify(config, null, 2));
-    LOG(`New alias ${alias} is added to the config`);
+    LOG(CH`{cyanBright New alias ${alias} is added to the Config}`);
   }
 };
 const cloneSourceProject = (args, resolve) => {
@@ -128,21 +128,21 @@ const cloneSourceProject = (args, resolve) => {
 
 
   SHELL.exec(`git clone ${sourceUrl} ${projectName}`);
-  LOG('Source project is cloned');
+  LOG(CH`{cyanBright Source project is cloned}`);
 
   SHELL.cd(`${projectName}`);
 
   if (CONFIG.clearHistory) {
     const isRemoved = SHELL.rm('-rf', '.git');
     if (isRemoved.code) {
-      ERR(`Can not remove ${'.git'}. It's possibly locked`);
+      ERR(CH`{red Can not remove ${'.git'}. It's possibly locked}`);
       SHELL.exit(0);
     }
 
     SHELL.exec('git init');
     SHELL.exec('git add .');
     SHELL.exec('git commit -m "Initial commit"');
-    LOG('Commits history of the source project is cleared');
+    LOG(CH`{cyanBright Commits history of the source project is cleared}`);
   }
 
   resolve(projectName);
@@ -157,14 +157,14 @@ const setNewGitOrigin = (args, resolve) => {
       SHELL.exec(`git remote set-url origin ${origin}`);
     }
 
-    LOG('Next step is forced push of Initial commit to the New Project\'s repo');
+    LOG(CH`{cyanBright Next step is a forced push of Initial commit to the New Project\'s repo}`);
     SHELL.exec('git push -u --force origin master');
     resolve();
   };
 
 
-  if (CONFIG.gitUriBase) {
-    const originUriFromConfig = CONFIG.gitUriBase.replace('%repo%', projectName);
+  if (CONFIG.gitUriTemplate) {
+    const originUriFromConfig = CONFIG.gitUriTemplate.replace('%repo%', projectName);
     setOrigin(originUriFromConfig);
   } else {
     const questions = [{
@@ -173,14 +173,33 @@ const setNewGitOrigin = (args, resolve) => {
       name: 'originUri',
     }];
     CLI.prompt(questions).then((answers) => {
-      LOG(CH`{blue Tip:} it's convenient to set up the "gitUriBase" in the Config file.`);
+      LOG(CH`{cyanBright Tip: it's convenient to set up the} "gitUriTemplate" {cyanBright in the Config file.}`);
       setOrigin(answers.originUri);
     });
   }
 };
 const replaceName = (args, resolve) => {
+  const oldName = JSON.parse(SHELL.cat('package.json')).name;
+
+  SHELL.sed('-i', oldName, PROJECT_NAME, 'package.json');
+  LOG(CH`{cyanBright The Source Project's name is replaced to the New Project's name in:} package.json`);
+
   resolve();
 };
+const runCommands = () => {
+  const { commands } = CONFIG;
+
+  if (commands && commands.length) {
+    LOG(CH`{cyanBright Next step: commands specified in the Config:}\n`, commands);
+
+    commands.forEach((command) => {
+      SHELL.exec(command);
+    });
+  }
+
+  SHELL.exit(0);
+};
+
 
 const roadmap = [
   [CONFIG_FILE],                      getConfig,
@@ -189,197 +208,6 @@ const roadmap = [
   [getSourceUrl, PROJECT_NAME],       cloneSourceProject,
   [cloneSourceProject, PROJECT_NAME], setNewGitOrigin,
   [setNewGitOrigin],                  replaceName,
+  [replaceName],                      runCommands,
 ];
 RUN(roadmap);
-
-
-/*
-
-
-if (process.argv.length < 4) {
-  console.log('Please provide an app name and a ZIP with the template');
-  console.log('e.g.');
-  console.log('$ craft MyApp https://github.com/stoyan/fail/archive/master.zip');
-  process.exit(1);
-}
-
-const fs = require('fs-extra');
-const path = require('path');
-const request = require('request');
-const url = require('url');
-const unzip = require('extract-zip');
-
-const stat = fs.statSync;
-
-const zip = process.argv[3];
-const app = process.argv[2];
-
-const replacebles = [
-  '.html',
-  '.css',
-  '.js',
-  '.json',
-].reduce((res, el) => {res[el] = 1; return res;}, {});
-
-let tempDir;
-let tempUnzipDir;
-const appDir = path.resolve(process.cwd(), app);
-
-log('Validating...');
-const uri = url.parse(zip);
-if (!uri.host || !uri.path || !uri.protocol) {
-  fail(zip, 'is not a valid URL');
-}
-
-try {
-  stat(appDir);
-  logError(appDir, 'already exists, giving up');
-  process.exit(1);
-} catch (_) {}
-
-log('Making app dirs...');
-fs.mkdirSync(appDir);
-tempDir = fs.mkdtempSync(appDir);
-tempUnzipDir = fs.mkdtempSync(tempDir);
-
-log('Downloading template...');
-const localZip = path.resolve(tempDir, 'template.zip');
-const file = fs.createWriteStream(localZip);
-
-request
-  .get(zip)
-  .on('error', err => fail(err))
-  .pipe(file);
-
-file.on('finish', () => {
-  file.close(() => {
-    let packageDir;
-    log('Unzipping...');
-    unzip(localZip, {
-        dir: tempUnzipDir,
-        onEntry: entry => {
-          if (entry.fileName.endsWith('package.json')) {
-            packageDir = path.resolve(tempUnzipDir, path.dirname(entry.fileName));
-          }
-        },
-      }, err => {
-        if (err) {
-          fail('Error unzipping, giving up', localZip);
-        }
-        if (!packageDir) {
-          fail('package.json missing from the template, giving up');
-        }
-        log('Configuring app...')
-        createApp(packageDir, appDir);
-    });
-  });
-});
-
-function createApp(source, dest) {
-  fs.copy(source, dest, (err) => {
-    if (err) {
-      fail(err);
-    }
-    const packageJson = path.resolve(dest, 'package.json');
-    const oldPackage = require(packageJson);
-
-    // replace all app names in all files
-    replaceFiles(dest, oldPackage.name, app);
-
-    // write package json
-    oldPackage.name = app;
-    oldPackage.version = '1.0.0';
-    fs.writeFile(packageJson, JSON.stringify(oldPackage, null, 2), (err) => {
-      if (err) {
-        logError(err);
-      }
-    });
-
-    // write readme
-    fs.writeFile(
-      path.resolve(dest, 'README.md'),
-      `# ${app}\n\nHello`,
-      () => {}
-    );
-
-    done();
-  });
-}
-
-function rmTemp() {
-  if (tempDir) {
-    fs.removeSync(tempDir);
-  }
-  if (tempUnzipDir) {
-    fs.removeSync(tempUnzipDir);
-  }
-}
-
-function replaceFiles(dir, seek, replaceWith) {
-  fs.readdirSync(dir).forEach(f => {
-    if (f.startsWith('.')) {
-      return; // no .DS_Store etc, thank you
-    }
-    if (f === 'package.json' || f.startsWith('README')) {
-      return; // special plan for these
-    }
-
-    const file = path.resolve(dir, f);
-    const stats = stat(file);
-
-    if (stats.isDirectory()) {
-      return replaceFiles(file, seek, replaceWith);
-    }
-    if (!replacebles[path.extname(f)]) {
-      return; // images and such
-    }
-
-    fs.readFile(file, 'utf-8', (err, contents) => {
-      if (err) {
-        logError(err);
-      }
-      contents = contents.replace(new RegExp(seek, 'g'), replaceWith);
-      fs.writeFile(file, contents, (err) => {
-        if (err) {
-          logError(err);
-        }
-      });
-    });
-
-  });
-}
-
-function fail(...msg) {
-  logError(msg.join(' '));
-  rmTemp();
-  if (appDir) {
-    fs.removeSync(appDir);
-  }
-  process.exit(1);
-}
-
-function log(...msg) {
-  console.log('\x1B[90m'+ msg.join(' ') +'\x1B[39m'); // thanks echomd
-}
-
-function logError(...msg) {
-  console.log('\x1B[31m✖ ' + msg.join(' ') + '\x1B[39m');
-}
-
-function done() {
-  console.log('\x1B[32m✔ Success\x1B[39m');
-  console.log('\nNext steps...');
-  console.log('  cd ' + app);
-  console.log('  npm install .');
-  rmTemp();
-
-  const postcraft = path.resolve(appDir, 'postcraft.txt');
-  fs.readFile(postcraft, 'utf-8', (err, contents) => {
-    if (err) {}
-    console.log('\nAnd a word from your template creator...');
-    console.log(contents);
-    fs.remove(postcraft);
-  });
-
-}
-*/
